f47f061c089969042638039369fc1824363ff668
nchchow <nelsonchow91.nc@gmail.com>
2020-05-12 15:40:52 -0700

    git ignore *.css.map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "./components/header/Header";
-import VideoPlayer from "./components/main/VideoPlayer";
-import VideoInfo from "./components/main/VideoInfo";
-import CommentsSection from "./components/main/CommentsSection";
-import VideoList from "./components/main/VideoList";
+import VideoPlayer from "./components/main-content/VideoPlayer";
+import VideoInfo from "./components/main-content/VideoInfo";
+import CommentsSection from "./components/main-content/CommentsSection";
+import VideoList from "./components/main-content/VideoList";
 
 export default class App extends React.Component {
 	render() {
