19c3d1bb5ac032c157ed900b2042b384fda9a7ae
nchchow <nelsonchow91.nc@gmail.com>
2020-05-12 18:42:12 -0700

    style video info

--- src/components/main-content/VideoInfo.js
+++ src/components/main-content/VideoInfo.js
@@ -1,25 +1,23 @@
 import React from "react";
+import ViewsIcon from "../../assets/icons/SVG/Icon-views.svg";
+import LikesIcon from "../../assets/icons/SVG/Icon-likes.svg";
 
 export default class VideoInfo extends React.Component {
 	render() {
 		return (
-			<div className="video-info">
+			<div className="video-info container">
 				<h1 className="video-info__title">BMX Rampage: 2018 Highlights</h1>
-				<h2 className="video-info__owner">By Red Cow</h2>
-				<h4 className="video-info__date-posted">12/18/2018</h4>
-				<img
-					src="../../assets/icons/SVG/Icon-views.svg"
-					alt=""
-					className="video-info__views--icon"
-				/>
-				<h4 className="video-info__views--count">1,001,023</h4>
-				<img
-					src="../../assets/icons/SVG/Icon-likes.svg"
-					alt=""
-					className="video-info__likes--icon"
-				/>
-				<h4 className="video-info__likes--count">110,985</h4>
-				<hr className="divider" />
+				<div className="video-info--primary">
+					<h2 className="video-info__owner">By Red Cow</h2>
+					<h4 className="video-info__date-posted">12/18/2018</h4>
+				</div>
+				<div className="video-info--secondary">
+					<img src={ViewsIcon} alt="views" className="video-info__icon" />
+					<h4 className="video-info__count">1,001,023</h4>
+					<img src={LikesIcon} alt="likes" className="video-info__icon" />
+					<h4 className="video-info__count">110,985</h4>
+				</div>
+				<hr className="video-info__divider" />
 				<h4 className="video-info__description">
 					On a gusty day in Southern Utah, a group of 25 daring mountain bikers
 					blew the doors off what is possible on two wheels, unleashing some of
