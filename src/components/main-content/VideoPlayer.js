30edb1f0c05aa8a2502c6b4f9713086c2fba70e3
nchchow <nelsonchow91.nc@gmail.com>
2020-05-12 17:37:22 -0700

    video player poster size

--- src/components/main-content/VideoPlayer.js
+++ src/components/main-content/VideoPlayer.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import VideoPlayerControls from "./VideoPlayerControls";
+import Poster from "../../assets/images/video-list-0.jpg";
 
-export default class VideoPlayer extends React.Component {
+export default class VideoPlayer extends Component {
 	render() {
 		return (
-			<video src="" className="video-player">
+			<video poster={Poster} src="" className="video-player">
 				<VideoPlayerControls />
 			</video>
 		);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-// import "./styles/main.css"
+import "./styles/main.css";
 
 ReactDOM.render(
 	<React.StrictMode>
