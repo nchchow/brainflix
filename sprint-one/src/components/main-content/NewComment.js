7fb56055d301063fa6d116e1daf0d9ddb6f8f877
nchchow <nelsonchow91.nc@gmail.com>
2020-05-14 14:52:05 -0700

    text size mixins work with all screen sizes

--- sprint-one/src/components/main-content/NewComment.js
+++ sprint-one/src/components/main-content/NewComment.js
@@ -7,14 +7,18 @@ export default function NewComment() {
 		<article className="new-comment">
 			<img src={UserIcon} alt="user" className="new-comment__user-icon" />
 			<form className="new-comment--form">
-				<h5 className="new-comment__content--header">JOIN THE CONVERSATION</h5>
-				<TextareaAutosize
-					name=""
-					id=""
-					maxRows={5}
-					className="new-comment__content"
-					placeholder="Add a comment"
-				></TextareaAutosize>
+				<div className="new-comment--content--wrapper">
+					<h5 className="new-comment__content--header">
+						JOIN THE CONVERSATION
+					</h5>
+					<TextareaAutosize
+						name=""
+						id=""
+						maxRows={5}
+						className="new-comment__content"
+						placeholder="Add a comment"
+					></TextareaAutosize>
+				</div>
 				<button className="new-comment__button">COMMENT</button>
 			</form>
 		</article>
