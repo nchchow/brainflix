9ee2e309b0083a6e67a983dfb168ce69f0d7d73c
nchchow <nelsonchow91.nc@gmail.com>
2020-05-14 18:05:44 -0700

    refactor to arrow function

--- sprint-one/src/components/sub-content/Comment.js
+++ sprint-one/src/components/sub-content/Comment.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-export default function Comment(props) {
-	return (
-		<>
-			<article className="comment">
-				<div className="comment--left">
-					<div className="comment__user-icon"></div>
+const Comment = (props) => (
+	<>
+		<article className="comment">
+			<div className="comment--left">
+				<div className="comment__user-icon"></div>
+			</div>
+			<div className="comment--right">
+				<div className="comment--right--top">
+					<span className="comment__name">{props.name}</span>
+					<span className="comment__date">{props.timestamp}</span>
 				</div>
-				<div className="comment--right">
-					<div className="comment--right--top">
-						<span className="comment__name">{props.name}</span>
-						<span className="comment__date">{props.timestamp}</span>
-					</div>
-					<p className="comment__content">{props.comment}</p>
-				</div>
-			</article>
-			<hr className="comment__divider" />
-		</>
-	);
-}
+				<p className="comment__content">{props.comment}</p>
+			</div>
+		</article>
+		<hr className="comment__divider" />
+	</>
+);
