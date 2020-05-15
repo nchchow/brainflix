89b6d79a8470f17467488bb954261e3afe203a98
nchchow <nelsonchow91.nc@gmail.com>
2020-05-15 11:58:52 -0700

    prevented page refresh when comment button is pressed

--- sprint-one/src/components/sub-content/NewCommentForm.js
+++ sprint-one/src/components/sub-content/NewCommentForm.js
@@ -2,20 +2,25 @@ import React from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import NewCommentBtn from "./NewCommentBtn";
 
-const NewCommentForm = () => (
-	<form className="new-comment--form">
-		<div className="new-comment--content--wrapper">
-			<h5 className="new-comment__content--header">JOIN THE CONVERSATION</h5>
-			<TextareaAutosize
-				name=""
-				id=""
-				maxRows={5}
-				className="new-comment__content"
-				placeholder="Add a comment"
-			></TextareaAutosize>
-		</div>
-		<NewCommentBtn />
-	</form>
-);
+const NewCommentForm = () => {
+	const formHandler = (event) => {
+		event.preventDefault();
+	};
+	return (
+		<form className="new-comment--form" onSubmit={formHandler}>
+			<div className="new-comment--content--wrapper">
+				<h5 className="new-comment__content--header">JOIN THE CONVERSATION</h5>
+				<TextareaAutosize
+					name=""
+					id=""
+					maxRows={5}
+					className="new-comment__content"
+					placeholder="Add a comment"
+				></TextareaAutosize>
+			</div>
+			<NewCommentBtn />
+		</form>
+	);
+};
 
 export default NewCommentForm;
