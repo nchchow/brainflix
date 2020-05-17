8da4da080768168cae118112ef00a32a00e6352c
nchchow <nelsonchow91.nc@gmail.com>
2020-05-17 13:01:23 -0700

    buttons for fullscreen and volume controls

--- sprint-one/src/components/main-video/VolumeBtn.js
+++ sprint-one/src/components/main-video/VolumeBtn.js
@@ -2,13 +2,13 @@ import React from "react";
 import volumeIcon from "../../assets/icons/SVG/Icon-volume.svg";
 
 const VolumeBtn = () => (
-	// <button className="video-player__controls--volume">
-	<img
-		src={volumeIcon}
-		alt=""
-		className="video-player__controls--volume--icon"
-	/>
-	// </button>
+	<button className="video-player__controls--volume">
+		<img
+			src={volumeIcon}
+			alt=""
+			className="video-player__controls--volume--icon"
+		/>
+	</button>
 );
 
 export default VolumeBtn;
