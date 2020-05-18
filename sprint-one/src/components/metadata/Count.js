b148e621d4c9a5eae56dcc879a96daa7b73c834f
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-18 10:02:54 -0700

    added alt attribute to silence warning

--- sprint-one/src/components/metadata/Count.js
+++ sprint-one/src/components/metadata/Count.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Count = ({ icon, value }) => (
   <>
-    <img src={icon} className="video-info__icon" />
+    <img src={icon} alt="" className="video-info__icon" />
     <h4 className="video-info__count">{value}</h4>
   </>
 );
