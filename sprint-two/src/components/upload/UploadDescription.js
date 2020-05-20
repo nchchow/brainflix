8cac2421afc9a2aecdbf442cd4a2546e77c0a1c7
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-19 17:44:48 -0700

    fixed extra line height underneath description text area

--- sprint-two/src/components/upload/UploadDescription.js
+++ sprint-two/src/components/upload/UploadDescription.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const UploadDescription = () => (
-  <label className="upload__description--label">
-    ADD A VIDEO DESCRIPTION
+  <>
+    <label for="upload__description" className="upload__description--label">
+      ADD A VIDEO DESCRIPTION
+    </label>
     <textarea
       name=""
-      id=""
+      id="upload__description"
       cols="30"
       rows="10"
       className="upload__description--textarea"
       placeholder="Add a description to your video"
     ></textarea>
-  </label>
+  </>
 );
 
 export default UploadDescription;
