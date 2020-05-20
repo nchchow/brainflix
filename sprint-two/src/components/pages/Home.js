3e6ba0a65a32fc98189759c70e2a2dec5cd76ee9
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-20 13:46:19 -0700

    videos data populated using api call

--- sprint-two/src/components/pages/Home.js
+++ sprint-two/src/components/pages/Home.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 import VideoPlayer from "../main-video/VideoPlayer";
 import VideoInfo from "../metadata/VideoInfo";
 import CommentsSection from "../sub-content/CommentsSection";
 import VideoList from "../sub-content/VideoList";
 
+const API_KEY = "14b626c6-5929-4318-9084-33bee95a76bc";
+const URL = "https://project-2-api.herokuapp.com";
+
 export default class Home extends Component {
+  state = {
+    mainVideo: {
+      id: "",
+      title: "",
+      description: "",
+      channel: "",
+      image: "",
+      views: "",
+      likes: "",
+      duration: "",
+      video: "",
+      timestamp: new Date().getTime(),
+      comments: [],
+    },
+    sideVideos: [],
+  };
+
+  componentDidMount = () => {
+    // initial api call
+    let id = "";
+    axios
+      .get(`${URL}/videos?api_key=${API_KEY}`)
+      .then((res) => {
+        id = res.data.shift().id; // get first id and remove from side videos
+        this.setState({ sideVideos: res.data });
+      })
+      .then(() => this.searchVideo(id)); // search for video content based on first id
+  };
+
+  searchVideo = (id) => {
+    axios
+      .get(`${URL}/videos/${id}?api_key=${API_KEY}`)
+      .then((res) => this.setState({ mainVideo: res.data }));
+  };
   render() {
     let {
       image,
@@ -16,7 +54,7 @@ export default class Home extends Component {
       likes,
       timestamp,
       comments,
-    } = this.props.mainVideo;
+    } = this.state.mainVideo;
     return (
       <>
         <VideoPlayer image={image} duration={duration} />
@@ -32,7 +70,7 @@ export default class Home extends Component {
             />
             <CommentsSection comments={comments} />
           </div>
-          <VideoList />
+          <VideoList sideVideos={this.state.sideVideos} />
         </main>
       </>
     );
--- sprint-two/src/components/sub-content/VideoList.js
+++ sprint-two/src/components/sub-content/VideoList.js
@@ -1,73 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import SideVideo from "./SideVideo";
 
-export default class VideoList extends Component {
-	state = {
-		sideVideos: [
-			{
-				id: 1,
-				title: "Become a Travel Pro In One Easy Lesson",
-				thumbnail: "./images/video-list-1.jpg",
-				channel: "Scotty Cranmer",
-			},
-			{
-				id: 2,
-				title: "Les Houches The Hidden Gem Of The Chamonix",
-				thumbnail: "./images/video-list-2.jpg",
-				channel: "Cornelia Blair",
-			},
-			{
-				id: 3,
-				title: "Travel Health Useful Medical Information For",
-				thumbnail: "./images/video-list-3.jpg",
-				channel: "Glen Harper",
-			},
-			{
-				id: 4,
-				title: "Cheap Airline Tickets Great Ways To Save",
-				thumbnail: "./images/video-list-4.jpg",
-				channel: "Emily Harper",
-			},
-			{
-				id: 5,
-				title: "Take A Romantic Break In A Boutique Hotel",
-				thumbnail: "./images/video-list-5.jpg",
-				channel: "Ethan Own",
-			},
-			{
-				id: 6,
-				title: "Choose The Perfect Accommodations",
-				thumbnail: "./images/video-list-6.jpg",
-				channel: "Lydia Perez",
-			},
-			{
-				id: 7,
-				title: "Cruising Destination Ideas",
-				thumbnail: "./images/video-list-7.jpg",
-				channel: "Timothy Austin",
-			},
-			{
-				id: 8,
-				title: "Train Travel On Track For Safety",
-				thumbnail: "./images/video-list-8.jpg",
-				channel: "Scotty Cranmer",
-			},
-		],
-	};
+const VideoList = ({ sideVideos }) => (
+  <aside className="video-list container">
+    <h5 className="video-list__header">NEXT VIDEO</h5>
+    {sideVideos.map((sideVideo) => (
+      <SideVideo
+        key={sideVideo.id}
+        title={sideVideo.title}
+        thumbnail={sideVideo.image}
+        channel={sideVideo.channel}
+      />
+    ))}
+  </aside>
+);
 
-	render() {
-		return (
-			<aside className="video-list container">
-				<h5 className="video-list__header">NEXT VIDEO</h5>
-				{this.state.sideVideos.map((sideVideo) => (
-					<SideVideo
-						key={sideVideo.id}
-						title={sideVideo.title}
-						thumbnail={sideVideo.thumbnail}
-						channel={sideVideo.channel}
-					/>
-				))}
-			</aside>
-		);
-	}
-}
+export default VideoList;
