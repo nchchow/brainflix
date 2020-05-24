b1319f6986ad2b1520642747958beddb0083f69c
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-24 14:40:15 -0700

    timestamp displays 'a moment ago' from 0 - 4 seconds, down from 0 - 10 seconds

--- sprint-two/src/scripts/dateUtil.js
+++ sprint-two/src/scripts/dateUtil.js
@@ -1,48 +1,48 @@
 const dateUtil = {
-	/*
+  /*
 		DATE FUNCTIONS
 	*/
-	// takes a timeStamp and returns date string in mm/dd/yyyy
-	getShortDate(timeStamp) {
-		// pad 0 for single digits
-		const padZero = (val) => {
-			return val < 9 ? "0" + val : val;
-		};
-		const date = new Date(timeStamp);
-		const m = padZero(date.getMonth() + 1);
-		const d = padZero(date.getDate());
-		const y = date.getFullYear();
-		return `${m}/${d}/${y}`;
-	},
+  // takes a timeStamp and returns date string in mm/dd/yyyy
+  getShortDate(timeStamp) {
+    // pad 0 for single digits
+    const padZero = (val) => {
+      return val < 9 ? "0" + val : val;
+    };
+    const date = new Date(timeStamp);
+    const m = padZero(date.getMonth() + 1);
+    const d = padZero(date.getDate());
+    const y = date.getFullYear();
+    return `${m}/${d}/${y}`;
+  },
 
-	// returns time elapsed in the largest appropriate unit as string
-	getTimeElapsed(timestamp) {
-		// take date difference and unit, then format to string
-		const formatDateDiff = (diff, unit) => {
-			if (diff !== 1) unit += "s"; // plural
-			return `${diff} ${unit} ago`;
-		};
+  // returns time elapsed in the largest appropriate unit as string
+  getTimeElapsed(timestamp) {
+    // take date difference and unit, then format to string
+    const formatDateDiff = (diff, unit) => {
+      if (diff !== 1) unit += "s"; // plural
+      return `${diff} ${unit} ago`;
+    };
 
-		const ms = new Date().getTime() - timestamp; // in milliseconds
-		const sec = ms / 1000;
-		if (sec < 10) return "a moment ago";
-		if (sec < 60) return formatDateDiff(Math.floor(sec), "second");
+    const ms = new Date().getTime() - timestamp; // in milliseconds
+    const sec = ms / 1000;
+    if (sec < 4) return "a moment ago";
+    if (sec < 60) return formatDateDiff(Math.floor(sec), "second");
 
-		const min = sec / 60;
-		if (min < 60) return formatDateDiff(Math.floor(min), "minute");
+    const min = sec / 60;
+    if (min < 60) return formatDateDiff(Math.floor(min), "minute");
 
-		const h = min / 60;
-		if (h < 24) return formatDateDiff(Math.floor(h), "hour");
+    const h = min / 60;
+    if (h < 24) return formatDateDiff(Math.floor(h), "hour");
 
-		const d = h / 24;
-		if (d < 30) return formatDateDiff(Math.floor(d), "day");
+    const d = h / 24;
+    if (d < 30) return formatDateDiff(Math.floor(d), "day");
 
-		const month = d / 30;
-		if (month < 12) return formatDateDiff(Math.floor(month), "month");
+    const month = d / 30;
+    if (month < 12) return formatDateDiff(Math.floor(month), "month");
 
-		const y = month / 12;
-		return formatDateDiff(Math.floor(y), "year");
-	},
+    const y = month / 12;
+    return formatDateDiff(Math.floor(y), "year");
+  },
 };
 
 const getShortDate = dateUtil.getShortDate;
