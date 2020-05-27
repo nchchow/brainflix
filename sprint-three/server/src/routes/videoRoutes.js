42755060eff35f8117ae3714fc957164941d22c2
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-26 18:45:48 -0700

    post video adds json file to videos dir

--- sprint-three/server/src/routes/videoRoutes.js
+++ sprint-three/server/src/routes/videoRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const postVideo = require("../controllers/postVideo");
+
 router.get("/", (req, res) => {
   const videos = require(`../models/videos.json`);
   res.json(videos);
@@ -9,6 +11,11 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   const video = require(`../models/videos/${req.params.id}.json`);
   res.json(video);
+});
+
+router.post("/", (req, res) => {
+  postVideo(req.body);
+  res.send("posted");
 });
 
 module.exports = router;
