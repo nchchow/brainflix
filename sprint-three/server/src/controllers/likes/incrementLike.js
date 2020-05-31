fb4538ce0aa5df3db053dc88088c29ee34755739
nelson chow <nelsonchow91.nc@gmail.com>
2020-05-30 22:09:09 -0700

    fixed bug where likes were stored as number rather than string

--- sprint-three/server/src/controllers/likes/incrementLike.js
+++ sprint-three/server/src/controllers/likes/incrementLike.js
@@ -4,7 +4,8 @@ const incrementLike = (id) => {
   // read video file
   const video = JSON.parse(fs.readFileSync(`./src/models/videos/${id}.json`));
   // increment like
-  video.likes++;
+  let likes = parseInt(video.likes.split(",").join("")) + 1;
+  video.likes = likes.toLocaleString();
   // write to file
   fs.writeFileSync(`./src/models/videos/${id}.json`, JSON.stringify(video));
   return video;
