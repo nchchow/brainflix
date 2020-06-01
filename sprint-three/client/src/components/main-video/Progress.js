6523e1d01ba04e61e9dda91d8368b822c5926f74
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-01 10:31:29 -0700

    scrubber moves along with progress

--- sprint-three/client/src/components/main-video/Progress.js
+++ sprint-three/client/src/components/main-video/Progress.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { formatTime } from "../../scripts/timeUtil";
+import scrubber from "../../assets/icons/SVG/Icon-scrubber-control.svg";
 
 const Progress = ({ currentTimeInSec, duration }) => {
+  const progRef = useRef();
   const progVal = () => {
     return typeof duration === "number" ? currentTimeInSec / duration : 0;
   };
   return (
     <div className="video-player__controls--progress">
       <progress
+        ref={progRef}
         className="video-player__controls--progress__loader"
         value={progVal()}
         min="0"
       ></progress>
+      <img
+        src={scrubber}
+        alt=""
+        className="video-player__controls--progress__loader--scrubber"
+        style={{
+          left: `${
+            progRef.current && progRef.current.offsetWidth * progVal() + 10
+          }px`,
+        }}
+      />
       <div className="video-player__controls--progress__duration">
         {formatTime(currentTimeInSec)} / {formatTime(duration)}
       </div>
